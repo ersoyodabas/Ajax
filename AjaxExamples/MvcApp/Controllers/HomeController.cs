f109280ac41185f8e57b0511b5a94e3c22d01243
Ersoy Odabas <ersoyodabas@outlook.com>
2019-05-16 17:50:41 +0300

    2

---
+++ AjaxExamples/MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApp.Models;
+
+namespace MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        Student st = new Student();
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetListStudent()  
+        {
+            return Json(Student.StudentList, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult InsertOrUpdateStudent(Student student)
+        {
+            Student.StudentList.Add(student);
+            return Json(Student.StudentList, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ AjaxExamples/MvcApp/Models/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string School { get; set; }
+        public string Class { get; set; }
+
+        public static List<Student> StudentList = new List<Student>
+        {
+            new Student
+            {
+                Id = 1,
+                Name = "Neslihan",
+                Surname = "Yağmur",
+                Age =22,
+                School = "İstanbul Üniv.",
+                Class = "11-A"
+            },
+            new Student
+            {
+                Id = 2,
+                Name = "Kemal",
+                Surname = "Türk",
+                Age = 23,
+                School = "Çorum Üniv.",
+                Class = "12-C"
+            }
+        };
+
+    }
+}
